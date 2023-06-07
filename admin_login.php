5143946dde85cb12acf3688680689e5c47247545
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-06-07 10:18:31 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,3 +1,65 @@
+
+<?php
+session_start();
+
+// Check if the user is already logged in
+if (isset($_SESSION['admin_username'])) {
+    // Redirect to admin dashboard if already logged in
+    header("Location: admin_dashboard.php");
+    exit();
+}
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Establish a database connection
+    $servername = "localhost";
+    $username = "blablaadmin";
+    $password = "bla123bla456";
+    $dbname = "prepit_users";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Check if username and password are provided
+    if (empty($_POST['admin_username']) || empty($_POST['admin_password'])) {
+        echo "Please enter username and password.";
+        exit();
+    }
+
+    // Get form data
+    $admin_username = mysqli_real_escape_string($conn, $_POST['admin_username']);
+    $admin_password = mysqli_real_escape_string($conn, $_POST['admin_password']);
+
+    // Query the database
+    $sql = "SELECT * FROM admin_users WHERE username='$admin_username' AND password='$admin_password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Check for query errors
+    if (!$result) {
+        die("Query error: " . mysqli_error($conn));
+    }
+
+    // Check if admin exists
+    if (mysqli_num_rows($result) == 1) {
+        // Admin authenticated, set session variable
+        $_SESSION['admin_username'] = $admin_username;
+
+        // Redirect to admin dashboard
+        header("Location: admin_dashboard.php");
+        exit();
+    } else {
+        // Admin authentication failed, show error message
+        $error_message = "Invalid username or password.";
+    }
+
+    mysqli_close($conn);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -76,8 +138,8 @@
                 <input type="submit" value="Login" class="btn">
             </div>
             <?php
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                echo '<div class="error-msg">Invalid username or password.</div>';
+            if (isset($error_message)) {
+                echo '<div class="error-msg">' . $error_message . '</div>';
             }
             ?>
         </form>
