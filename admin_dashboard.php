47b5429a3b86e2251e00e8cb39585302c741ce65
Shobin <130288084+Mac-Rughs@users.noreply.github.com>
2023-06-07 11:22:11 +0530

    sugam

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -22,12 +22,47 @@ if (isset($_GET['logout'])) {
 <head>
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="styles.css" type="text/css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 100px;
+            text-align: center;
+        }
+
+        h1 {
+            margin-bottom: 30px;
+            color: #333333;
+        }
+
+        .logout-link {
+            display: block;
+            margin-top: 20px;
+            color: #4CAF50;
+            text-decoration: none;
+        }
+
+        .logout-link:hover {
+            color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h1>Welcome, <?php echo $_SESSION['admin_username']; ?></h1>
+    <div class="container">
+        <h1>Welcome, <?php echo $_SESSION['admin_username']; ?></h1>
 
-    <!-- Add your dashboard content here -->
+        <!-- Add your dashboard content here -->
 
-    <a href="?logout=1">Logout</a>
+        <a href="?logout=1" class="logout-link">Logout</a>
+    </div>
 </body>
 </html>
