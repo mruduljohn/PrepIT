2df7311258ae255e0141612d291005a556c74408
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-06-07 10:40:02 +0530

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -6,29 +6,8 @@ if (isset($_SESSION['admin_username'])) {
     // Display the file upload form
     include 'upload_notes.php';
 } else {
-    // If not logged in, display the admin login form
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Admin Dashboard</title>
-        <link rel="stylesheet" href="styles.css" type="text/css">
-    </head>
-    <body>
-        <h1>Admin Login</h1>
-
-        <!-- Admin Login Form -->
-        <form action="admin_login.php" method="POST">
-            <label for="admin_username">Username:</label>
-            <input type="text" id="admin_username" name="admin_username" required>
-
-            <label for="admin_password">Password:</label>
-            <input type="password" id="admin_password" name="admin_password" required>
-
-            <button type="submit">Login</button>
-        </form>
-    </body>
-    </html>
-    <?php
+    // If not logged in, redirect to the admin login page
+    header("Location: admin_login.php");
+    exit();
 }
 ?>
