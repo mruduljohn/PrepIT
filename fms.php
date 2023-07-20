f684b1bce25b886761d9a1150b4c474c028cb2c2
Shobin <130288084+Mac-Rughs@users.noreply.github.com>
2023-07-20 19:03:11 +0530

    20.7 pms fms css

--- fms.php
+++ fms.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
-    <title>PrepIT Full Mark</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <title>PHP Web Page</title>
+    
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta name="description"  content="PrepIT"/>
     <meta name="keywords" content="PrepIT" />
@@ -13,46 +14,9 @@
     <!-- Bootstrap CSS -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css"> -->
     <link href="css/main.css" rel="stylesheet" type="text/css"/>
-    <link rel="shortcut icon" type="image/png" href="images/header/favicon.png" />
-    <style>
-        /* Your custom CSS styles */
-        body {
-            font-family: 'Roboto', sans-serif;
-        }
-        .Full_mark {
-            text-align: center;
-            margin-top: 20px;
-        }
-        .dropdown {
-            display: inline-block;
-            margin: 20px;
-        }
-        .dropdown .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            padding: 12px 16px;
-            z-index: 1;
-        }
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-        .back-button {
-            text-align: center;
-            margin-top: 20px;
-
-        }
-        .btn {
-            font-size: 30px;
-            padding: 15px 30px;
-        }
-        .btn-secondary {
-            color: #f5f5f5;
-            background-color: #06233d;
-        }
-    </style>
+    
+    
+    
 </head>
 <body>
 <div id="educo_wrapper">
@@ -105,143 +69,115 @@
   </header>
 </div>
 <!--Header end-->
+<div class="Full_mark">
 
-    <div class="container">
-        <div class="Full_mark">
-            <h1 class="display-3">FULL MARK ORIENTED</h1>
-        </div>
-        <br><br><br>
-        <div class="row justify-content-center">
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem1Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 1
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem1Dropdown">
-                        <li><a class="dropdown-item" href="#">Calculus</a></li>
-                        <li><a class="dropdown-item" href="#">Engineering Physics</a></li>
-                        <li><a class="dropdown-item" href="#">Engineering Mechanics</a></li>
-                        <li><a class="dropdown-item" href="#">Basic Civil Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Basic Mechanical Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Soft Skills Development</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6"> 
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem2Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 2
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem2Dropdown">
-                        <li><a class="dropdown-item" href="#">Computer Programming</a></li>
-                        <li><a class="dropdown-item" href="#">Engineering Chemistry</a></li>
-                        <li><a class="dropdown-item" href="#">Engineering Graphics</a></li>
-                        <li><a class="dropdown-item" href="#">Basic Electrical Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Basic Electronics Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Environmental Studies</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem3Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 3
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem3Dropdown">
-                        <li><a class="dropdown-item" href="resource.php?subject=Latt & firstLetter=F">Linear Algebra and Transform Techniques</a></li>
-                        <li><a class="dropdown-item" href="resource.php?subject=DE & firstLetter=F">Digital Electronics</a></li>
-                        <li><a class="dropdown-item" href="resource.php?subject=DSA & firstLetter=F">Data Structures and Algorithms</a></li>
-                        <li><a class="dropdown-item" href="resource.php?subject=DBMS & firstLetter=F">Database Management System</a></li>
-                        <li><a class="dropdown-item" href="resource.php?subject=DCS & firstLetter=F">Discrete Computational Structures</a></li>
-                        <li><a class="dropdown-item" href="resource.php?subject=CAO & firstLetter=F">Computer Architecture and Organization</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem4Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 4
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem4Dropdown">
-                        <li><a class="dropdown-item" href="#">Complex Variables and Partial Differential Equations</a></li>
-                        <li><a class="dropdown-item" href="#">Microprocessors</a></li>
-                        <li><a class="dropdown-item" href="#">Object Oriented Programming</a></li>
-                        <li><a class="dropdown-item" href="#">Automata Languages and Computations</a></li>
-                        <li><a class="dropdown-item" href="#">Data Structures and Algorithms</a></li>
-                        <li><a class="dropdown-item" href="#">Principles of Programming</a></li>
-                        <li><a class="dropdown-item" href="#">Universal Human Values</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem5Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 5
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem5Dropdown">
-                        <li><a class="dropdown-item" href="#">Numerical and Statistical Methods</a></li>
-                        <li><a class="dropdown-item" href="#">System Programming</a></li>
-                        <li><a class="dropdown-item" href="#">Object Oriented Software Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Computer Graphics</a></li>
-                        <li><a class="dropdown-item" href="#">Advanced Microprocessors and Microcontrollers</a></li>
-                        <li><a class="dropdown-item" href="#">Professional Elective I</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem6Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 6
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem6Dropdown">
-                        <li><a class="dropdown-item" href="#">Computer Networks</a></li>
-                        <li><a class="dropdown-item" href="#">Compiler Construction</a></li>
-                        <li><a class="dropdown-item" href="#">Analysis and Design of Algorithms</a></li>
-                        <li><a class="dropdown-item" href="#">Data Mining</a></li>
-                        <li><a class="dropdown-item" href="#">Operating System</a></li>
-                        <li><a class="dropdown-item" href="#">Professional Elective II</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem7Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 7
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem7Dropdown">
-                        <li><a class="dropdown-item" href="#">Principles of Management</a></li>
-                        <li><a class="dropdown-item" href="#">Advanced Architecture and Parallel Processing</a></li>
-                        <li><a class="dropdown-item" href="#">Cryptography and Network Security</a></li>
-                        <li><a class="dropdown-item" href="#">Professional Elective III</a></li>
-                        <li><a class="dropdown-item" href="#">Operating System</a></li>
-                        <li><a class="dropdown-item" href="#">Open Elective I</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="sem8Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        SEM 8
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="sem8Dropdown">
-                        <li><a class="dropdown-item" href="#">Artificial Intelligence</a></li>
-                        <li><a class="dropdown-item" href="#">Professional Elective IV</a></li>
-                        <li><a class="dropdown-item" href="#">Professional Elective V</a></li>
-                        <li><a class="dropdown-item" href="#">Open Elective II</a></li>
-                        <li><a class="dropdown-item" href="#">Operating System</a></li>
-                        <li><a class="dropdown-item" href="#">Seminar</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+<link rel="stylesheet" href="css/fms2.css" type="text/css">
+    <div> <h1 class="display-3">FULL MARK ORIENTED</h1></div>
+    <div class="noteSection">
+
+    <div class="container1">
+      <div class="dropdown">
+          <h2><a class="Sem"> SEM 1 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Calculus</a>
+                      <a href="#">Engineering Physics</a>
+                      <a href="#">Engineering Mechanics</a>
+                      <a href="#">Basic Civil Engineering</a>
+                      <a href="#">Basic Mechanical
+      Engineering</a>
+                      <a href="#">Soft Skills Development </a>
+          </div>
+      </div>
+      <div class="dropdown">
+          <h2><a class="Sem">   SEM 2 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Computer Programing</a>
+                      <a href="#">Engineering Chemistry</a>
+                      <a href="#">Engineering Graphics</a>
+                      <a href="#">Basic Electrical Engineering</a>
+                      <a href="#">Basic Electronics Engineering</a>
+                      <a href="#">Environmental Studies</a>
+          </div>
+      </div> <div class="dropdown">
+          <h2><a class="Sem">   SEM 3 </a> </h2>
+          <div class="dropdown-content">
+          <a href="resource.php?subject=Latt & firstLetter=F">Linear Algebra and Transform Techniques</a>
+          <a href="resource.php?subject=DE & firstLetter=F">Digital Electronics</a>
+          <a href="resource.php?subject=DSA & firstLetter=F">Data Stuctures and Algorithms</a>
+          <a href="resource.php?subject=DBMS & firstLetter=F">Database Management System</a>
+          <a href="resource.php?subject=DCS & firstLetter=F">Discrete Computational Structures</a>
+          <a href="resource.php?subject=CAO & firstLetter=F">Computer Architecture and Organization</a>
+      </div>
+      </div> <div class="dropdown">
+          <h2><a class="Sem">   SEM 4 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Complex Variables and Partial
+      Differential Equations </a>
+                      <a href="#">Microprocessors</a>
+                      <a href="#">Computer Architecture and
+      Organization </a>
+                      <a href="#">Automata Languages and
+      Computations</a>
+                      <a href="#">Data Structures and Algorithms</a>
+                      <a href="#">Database Management Systems</a>
+                      <a href="#">Universal Human Values</a>
+          </div>
+      </div> <div class="dropdown">
+          <h2><a class="Sem">   SEM 5 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Numerical and Statistical
+      Methods</a>
+                      <a href="#">System Programming</a>
+                      <a href="#">Object Oriented Software
+      Engineering</a>
+                      <a href="#">Computer Graphics</a>
+                      <a href="#">Advanced Microprocessors and
+      Microcontrollers </a>
+                      <a href="#">Professional Elective I </a>
+          </div>
+      </div> <div class="dropdown">
+          <h2><a class="Sem">   SEM 6 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Computer Networks </a>
+                      <a href="#">Compiler Construction</a>
+                      <a href="#">Analysis and Design of
+      Algorithms</a>
+                      <a href="#">Data Mining</a>
+                      <a href="#">Operating System</a>
+                      <a href="#">Professional Elective II</a>
+          </div>
+      </div>
+      
+      <div class="dropdown">
+          <h2><a class="Sem">   SEM  7</a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Principles of Management </a>
+                      <a href="#">Advanced Architecture and Parallel
+      Processing</a>
+                      <a href="#">Cryptography and Network Security</a>
+                      <a href="#">Professional Elective III</a>
+                      <a href="#">Operating System</a>
+                      <a href="#">Open Elective I</a>
+          </div>
+      </div>
+      
+      <div class="dropdown">
+          <h2><a class="Sem">   SEM 8 </a> </h2>
+          <div class="dropdown-content">
+                      <a href="#">Artificial Intelligence  </a>
+                      <a href="#">Professional Elective IV</a>
+                      <a href="#">Professional Elective V</a>
+                      <a href="#">Open Elective II</a>
+                      <a href="#">Operating System</a>
+                      <a href="#">Seminar</a>
+          </div>
+      </div>
     </div>
-    <div class="back-button">
-        <a class="btn btn-secondary" href="dashboard.php">Back</a>
-    </div>
-    
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+</div>
+
+</div>  
+<div class="ed_info_wrapper a, back-button">
+<a href="dashboard.php" id="login_button">Back</a>
+</div>
 </body>
 </html>
