547ec01b5f114341888c21edcd2ed5873503b7be
Sandesh R <2getsandesh@gmail.com>
2023-07-30 12:54:20 +0530

    upload profile pic

--- upload_dp.php
+++ upload_dp.php
@@ -1,16 +1,20 @@
 <?php
+    // Get the username from the session
+    session_start();
+    $username_user = $_SESSION['username'];
+
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if a file is selected
     if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
         // Establish a database connection
         $servername = "localhost";
-        $username = "blablaadmin";
-        $password = "bla123bla456";
+        $username_DB = "blablaadmin";
+        $password_DB = "bla123bla456";
         $dbname = "prepit_users";
 
         // Create a new connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $username_DB, $password_DB, $dbname);
 
         // Check connection
         if (!$conn) {
@@ -23,25 +27,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $file_type = $_FILES['image']['type'];
         $file_size = $_FILES['image']['size'];
 
+        //prepare file for insertion
+        $file_name = mysqli_real_escape_string($conn, $file_name);
+
         // Folder configuration
         $folder = "profile_photo/";
         $file_path = $folder . $file_name;
 
         // Move the uploaded file to the desired directory
         if (move_uploaded_file($file_tmp, $file_path)) {
-            // Get the username from the session
-            session_start();
-            $username_user = $_SESSION['username'];
+
 
             // Prepare the data for insertion
             $file_path = mysqli_real_escape_string($conn, $file_path);
 
             // Update the 'profile_photo' column in the 'users' table
-            $sql = "UPDATE users SET profile_photo = '$file_path' WHERE username = '$username_user'";
+            $sql = "UPDATE users SET pic_path = '$file_path' WHERE username = '$username_user'";
             if (mysqli_query($conn, $sql)) {
                 echo "Image uploaded successfully.";
-                header("Location: upload_dp.php"); // Redirect to the desired page
-                exit; // Terminate the current script
+                // Set the 'pic_path' session variable to the file path
+                $_SESSION['pic_path'] = $file_path;
+                echo $_SESSION['pic_path'];
+                
             } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
@@ -66,6 +73,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
-    <h2>Upload Pro pic</h2>
+<style>
+                .back-button {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            color: #ffffff;
+            background-color: #343a40;
+            padding: 10px 20px;
+            font-weight: bold;
+            border-radius: 30px;
+            text-decoration: none;
+        }
+    </style>
+<a class="back-button" href="dashboard.php">Back</a>
 </body>
 </html>
