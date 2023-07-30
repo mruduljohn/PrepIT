6e7ae841b714f081e11bb5798abc0032ba20ad2b
Nandana Santhosh <nandanapsmail@gmail.com>
2023-07-30 14:09:19 +0530

    simple css added  to uploaded successful pages

--- upload_dp.php
+++ upload_dp.php
@@ -44,13 +44,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Update the 'profile_photo' column in the 'users' table
             $sql = "UPDATE users SET pic_path = '$file_path' WHERE username = '$username_user'";
             if (mysqli_query($conn, $sql)) {
-                echo "Image uploaded successfully.";
+                echo '<div class="message-container success-message">Image uploaded successfully.</div>';
                 // Set the 'pic_path' session variable to the file path
                 $_SESSION['pic_path'] = $file_path;
-                echo $_SESSION['pic_path'];
+                
                 
             } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                echo '<div class="message-container error-message">Error: ' . $sql . '<br>' . mysqli_error($conn) . '</div>';
             }
         } else {
             echo "Error uploading file.";
@@ -85,6 +85,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             border-radius: 30px;
             text-decoration: none;
         }
+        .message-container {
+  background-color: #cce5ff;
+  color: #004085;
+  padding: 10px;
+  border: 1px solid #b8daff;
+  border-radius: 4px;
+  margin: 20px auto;
+  max-width: 400px;
+  text-align: center;
+}
+
     </style>
 <a class="back-button" href="dashboard.php">Back</a>
 </body>
